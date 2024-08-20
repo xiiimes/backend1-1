1436e3f3bf393bd0d174283532681a4a169e63d9
CristianHerrada-CH <cristianherrada12@gmail.com>
2024-08-20 07:27:59 -0500

    clase20deagosto2024
    clase20deagosto2024

---
+++ Backend1MJ6AM2004/src/notasjava/Operator.java
@@ -0,0 +1,17 @@
+package notasjava;
+
+public class Operator {
+        public static void main(String[] args) {
+            //aritmeticos (+,-,/,*,%)
+
+            int num1 = 50;
+
+            int num2 = 35;
+
+            int sum = num1 + num2;
+
+            System.out.println(sum);
+        }
+    }
+
+
