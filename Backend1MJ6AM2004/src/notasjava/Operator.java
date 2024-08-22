1514af1648c2ce151bd16cd670df91aaa9ff0523
CristianHerrada-CH <cristianherrada12@gmail.com>
2024-08-22 07:35:50 -0500

    220824

--- Backend1MJ6AM2004/src/notasjava/Operator.java
+++ Backend1MJ6AM2004/src/notasjava/Operator.java
@@ -4,13 +4,19 @@ public class Operator {
         public static void main(String[] args) {
             //aritmeticos (+,-,/,*,%)
 
-            int num1 = 50;
+            int num1 = 21;
 
-            int num2 = 35;
+            int num2 = 2;
 
             int sum = num1 + num2;
 
             System.out.println(sum);
+
+            System.out.println("Resta =" + rest);
+            System.out.println("Multi =" + mult);
+            System.out.println("Div =" + div);
+            System.out.println("Mod = " + mod);
+
         }
     }
 
